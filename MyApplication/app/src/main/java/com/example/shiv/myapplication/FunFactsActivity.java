e8d093b2cba8056f2aeab0f36cb8789df7d3d556
Shiv Kumar <championballer08@gmail.com>
2018-09-21 16:37:17 +0530

    Added dynamic colors

--- MyApplication/app/src/main/java/com/example/shiv/myapplication/FunFactsActivity.java
+++ MyApplication/app/src/main/java/com/example/shiv/myapplication/FunFactsActivity.java
@@ -1,9 +1,11 @@
 package com.example.shiv.myapplication;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import java.util.Random;
@@ -12,8 +14,10 @@ public class FunFactsActivity extends AppCompatActivity {
     // Declare our view variables
     //private String[] facts = {"Shiv is the best","Shiv loves Agrima","Shiv will fight till the very end","Shiv will be patient","Shiv will try and keep getting better"};
     private FactBook factBook;
+    private ColorDir colorDir;
     private TextView mFactTextView;
     private Button mShowFactButton;
+    private RelativeLayout layout;
     //private int index = 0;
 
 
@@ -26,6 +30,8 @@ public class FunFactsActivity extends AppCompatActivity {
         mFactTextView = findViewById(R.id.factTextView);
         mShowFactButton = findViewById(R.id.showFactButton);
         factBook = new FactBook();
+        colorDir = new ColorDir();
+        layout = findViewById(R.id.main);
         View.OnClickListener listener = new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -38,6 +44,8 @@ public class FunFactsActivity extends AppCompatActivity {
 
                 String fact = factBook.getFact();
                 mFactTextView.setText(fact);
+                int color = colorDir.getColor();
+                layout.setBackgroundColor(color);
             }
         };
 
